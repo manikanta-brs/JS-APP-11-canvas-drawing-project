b50b2adf4fcff153194f0c70ceed7d66e6bf9830
manikanta-brs <manikantadon0000@gmail.com>
2024-11-11 16:04:29 +0530

    first - commit

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+const canvas = document.getElementById("draw");
+// canvas.width = canvas.clientWidth;
+// canvas.height = canvas.clientHeight;
+
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 10;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+function draw(e) {
+  if (!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+  ctx.beginPath();
+
+  ctx.moveTo(lastX, lastY);
+
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
+
+function setTheme(theme) {
+  document.documentElement.style.setProperty("--primary-color", theme);
+  localStorage.setItem("movie-theme", theme);
+}
+setTheme(localStorage.getItem("movie-theme") || chathams_blue);
